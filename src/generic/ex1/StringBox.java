fd26da8078b03617208878264bce15399a5932b0
SsangSoo <kss10080@naver.com>
2025-01-01 05:21:31 +0900

    sec01 - 01 complete

---
+++ src/generic/ex1/StringBox.java
@@ -0,0 +1,15 @@
+package generic.ex1;
+
+// 문자열을 보관하는 객체
+public class StringBox {
+
+    private String value;
+
+    public void set(String value) {
+        this.value = value;
+    }
+
+    public String get() {
+        return value;
+    }
+}
